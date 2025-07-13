f82d2eab426205e5cc8bca782fd6e2a7ca687026
Malik Hamlaoui <kilam1@hotmail.fr>
2025-07-13 13:48:25 +0200

    last

--- migrations/Version20250713114556.php
+++ migrations/Version20250713114556.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250713111632 extends AbstractMigration
+final class Version20250713114556 extends AbstractMigration
 {
     public function getDescription(): string
     {
